3bfecf0187202cf043259185e5d87890122afe31
Borunov Semyon <1132226476@pfur.ru>
2022-11-25 13:42:26 +0300

    2

---
+++ labs/lab07/lab07/lab7-4.asm
@@ -0,0 +1,33 @@
+%include 'in_out.asm'
+
+SECTION .data
+msg: DB 'введите х: ',0
+
+SECTION .bss
+x: RESB 80
+
+SECTION .text
+
+GLOBAL _start
+_start:
+mov eax, msg        
+call sprintLF		;вывод приглашения
+mov ecx, x
+mov edx, 80
+call sread		;чтение в переменную х
+mov eax,x
+call atoi		;делаем переменную правильного формата
+
+inc eax			;добавили единицу
+mov ebx,18
+mul ebx			;умножение на 18
+
+mov ebx,6
+div ebx			;деление на 6
+
+mov eax,msg
+call sprint 
+mov eax,edx
+call iprintLF
+
+call quit
